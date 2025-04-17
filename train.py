736ca8b9be04395462f6ab8b47fdaf189ef2b48a
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2025-04-17 16:17:35 -0300

    Update train.py

--- train.py
+++ train.py
@@ -220,7 +220,7 @@ def train_classification_model(curr_exp, model, dataloaders, dataset_sizes, crit
                         # Save the best model weights to disk
                         torch.save(best_model_wts, model_checkpoint_path / "best_model.pth")
             
-            plot_training_curves(training_curves, CURRENT_EXP)
+            plot_training_curves(training_curves, config.exp)
 
     except KeyboardInterrupt:
         logger.debug("Training interrupted. Saving best model and results so far...")
